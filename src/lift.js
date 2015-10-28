0c6f8e84a30f3f94fd13e45ed6d1c4a002f73c17
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-10-28 16:41:24 -0700

    Removed unused junk.

--- src/lift.js
+++ src/lift.js
@@ -1,18 +1,4 @@
 
-import { compose } from 'redux';
-
-type LiftArguments = {
-  reducer: Function,
-  initialState: Function,
-  dispatch: Function
-};
-
-export const ActionTypes = {
-  INIT: '@@redux/INIT',
-  PROXY: '@@redux/PROXY'
-};
-
-
 /**
  * [initialState description]
  * @param   {[type]} liftInitialState [description]
@@ -27,35 +13,6 @@ export function liftState(initialState, liftedInitialState) {
 
 export function unliftState(state) {
   return state;
-}
-
-/**
- * [dispatch description]
- * @param   {[type]} liftDispatch [description]
- * @returns {[type]}              [description]
- */
-export function liftStore(store, liftedStore) {
-  return {
-    ...store,
-    parent: store,
-    root: store.root || store,
-    dispatch(action) {
-      store.dispatch({
-        type: ActionTypes.PROXY,
-        target: 1,
-        action,
-      })
-    }
-  };
-}
-
-function unliftStore(store) {
-  return {
-    ...store,
-    getState() {
-      return store.getState()[0];
-    }
-  };
 }
 
 export function liftAction(type, action) {
