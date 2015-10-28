81043a9ae03f0e5785a1d2a4e69e0c92c374eb76
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-10-28 16:04:24 -0700

    Better tests maybe.

--- test/spec/lift.spec.js
+++ test/spec/lift.spec.js
@@ -15,81 +15,91 @@ import {
 
 chai.use(sinonChai);
 
+// Simple reducer.
+function app(state, { type, value }) {
+  return type === 'UPDATE' ? state + value : state;
+}
+
+function reducerX(reducer) {
+  return (state, action) => {
+    const [a,b] = unliftState(state);
+    switch(action.type) {
+    case 'CHILD':
+      return liftState(reducer(a, unliftAction(action)), b);
+    case 'MESSAGE':
+      return liftState(a, { message: action.payload });
+    default:
+      return state;
+    }
+  }
+}
+
+function initialStateX(initialState) {
+  return liftState(initialState, { message: 'hello' });
+}
+
+function storeX(store) {
+  return {
+    ...store,
+    parent: {
+      ...store,
+      replaceReducer() {
+        throw new TypeError();
+      },
+      getState() {
+        const [a,b] = unliftState(store.getState());
+        return a;
+      },
+      dispatch(action) {
+        return store.dispatch(liftAction('CHILD', action));
+      }
+    },
+    replaceReducer(reducer) {
+      return store.replaceReducer(reducerX(reducer));
+    }
+  }
+}
+
+function enhancer(next : Function) : Function {
+  return (reducer : Function, initialState : any) => {
+    return storeX(next(reducerX(reducer), initialStateX(initialState)));
+  }
+}
+
 describe('lift', () => {
   it('should return a store-creator function', () => {
-
+    const creator = enhancer(createStore);
+    expect(creator).to.be.a.function;
+    expect(creator(() => {})).to.have.property('dispatch');
   });
 
-  it('should allow parents to forward reduces to children', () => {
 
+  describe('parent stores', () => {
+    it('should exist', () => {
+      const liftedCreateStore = compose(enhancer)(createStore);
+      const store = liftedCreateStore(app, 1);
+      expect(store).to.have.property('parent');
+    });
+
+    it('should return parent state', () => {
+      const liftedCreateStore = compose(enhancer)(createStore);
+      const store = liftedCreateStore(app, 6);
+      expect(store.parent.getState()).to.equal(6);
+    });
+
+    it('should dispatch correctly', () => {
+      const liftedCreateStore = compose(enhancer)(createStore);
+      const store = liftedCreateStore(app, 1);
+      store.parent.dispatch({ type: 'UPDATE', value: 5 });
+      expect(store.getState()).to.have.property(0, 6);
+    });
   });
 
-  it('should expose parent stores', () => {
-    // store.unlift();
-  });
-
-  it('should expose the root store', () => {
-
-  });
 
   it('should do something', () => {
 
-    // Simple reducer.
-    function app(state, { type, value }) {
-      return type === 'UPDATE' ? state + value : state;
-    }
-
-    function reducerX(reducer) {
-      return (state, action) => {
-        const [a,b] = unliftState(state);
-        switch(action.type) {
-        case 'CHILD':
-          return liftState(reducer(a, unliftAction(action)), b);
-        case 'MESSAGE':
-          return liftState(a, { message: action.payload });
-        default:
-          return state;
-        }
-      }
-    }
-
-    function initialStateX(initialState) {
-      return liftState(initialState, { message: 'hello' });
-    }
-
-    function storeX(store) {
-      return {
-        ...store,
-        parent: {
-          ...store,
-          getState() {
-            const [a,b] = unliftState(store.getState());
-            return a;
-          },
-          dispatch(action) {
-            return store.dispatch(liftAction('CHILD', action));
-          }
-        },
-        replaceReducer(reducer) {
-          return store.replaceReducer(reducerX(reducer));
-        }
-      }
-    }
-
-    function enhancer(next : Function) : Function {
-      return (reducer : Function, initialState : any) => {
-        return storeX(next(reducerX(reducer), initialStateX(initialState)));
-      }
-    }
-
-    // Create the store.
-    const liftedCreateStore = compose(
-      enhancer
-    )(createStore);
-
+    const liftedCreateStore = compose(enhancer)(createStore);
     const store = liftedCreateStore(app, 1);
-
-
 
     store.parent.dispatch({ type: 'UPDATE', value: 5 });
     store.dispatch({ type: 'MESSAGE', payload: 'world' });
